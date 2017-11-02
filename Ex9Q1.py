7abf2e2e50e7839a3680bc732336082ad8759354
Phil McCown <pjlmac@gmail.com>
2017-11-02 11:01:08 -0400

    Reworking the def's

--- Ex9Q1.py
+++ Ex9Q1.py
@@ -8,21 +8,22 @@ data.columns
 subset1=data.loc[data.mutation.isin(['WT','M124K']),:]
 subset2=data.loc[data.mutation.isin(['WT','V456D']),:]
 subset3=data.loc[data.mutation.isin(['WT','I213N']),:]
-def null (p,obs):
-    B0=p[0]
-    sigma=p[1]
-    expected=B0
-    nll=-1*norm(expected,sigma).logpdf(obs.y).sum()
-    return nll
-def alter(p,obs):
-    B0=p[0]
-    B1=p[1]
-    sigma=p[2]
-    expected=B0+B1*obs.x
-    nll=-1*norm(expected,sigma).logpdf(obs.y).sum()
-    return nll
-initialGuess=numpy.array([1,1,1])
-fitNull=minimize(null,initialGuess,method="Nelder-Mead",options={'disp':True},args=subset1)
-fitAlter=minimize(alter,initialGuess,method="Nelder-Mead",options={'disp':True},args=subset1)
+
+#def mean (p,obs):
+ #   B0=y[0]
+ #   sigma=y[1]
+ #   expected=B0
+ #   nll=-1*norm(expected,sigma).logpdf(obs.y).sum()
+ #   return nll
+#def like (p,obs):
+ #   B0=p[0]
+ #   B1=p[1]
+ #   sigma=p[2]
+ #   expected=B0+B1*obs.x
+ #   nll=-1*norm(expected,sigma).logpdf(obs.y).sum()
+ #   return nll
+#initialGuess=numpy.array([1,1,1])
+#fitNull=minimize(null,initialGuess,method="Nelder-Mead",options={'disp':True},args=subset1)
+#fitAlter=minimize(alter,initialGuess,method="Nelder-Mead",options={'disp':True},args=subset1)
 #D=2*(fitNull.fun-fitAlter.fun)
 #1-scipy.stats.chi2.cdf(x=D,df=1)
