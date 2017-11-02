f71f8fa416702bba61ae8f1586954c7decabd111
Phil McCown <pjlmac@gmail.com>
2017-11-02 15:24:18 -0400

    Cleaned most of Q3; getting an odd error message though

--- Ex9Q3.py
+++ Ex9Q3.py
@@ -1,3 +1,8 @@
+import numpy
+import pandas
+import scipy
+from scipy.optimize import minimize
+from scipy.stats import norm
 #add'leafDecomp.csv'
 ld=pandas.read_csv("leafDecomp.csv",header=0)
 
@@ -10,12 +15,11 @@ leaves.head()
 def constant(p,obs):
     B0=p[0]
     sigma=p[1]
-    
     expected=B0
-    nll=-1*norm(expected,sigma).logpdf(obs.y).sum
+    nll=-1*norm(expected,sigma).logpdf(obs.y).sum()
     return nll
     
-#set intial guess
+#set initial guess
 constantGuess=numpy.array([600,1])
 
 #estimate parameters
@@ -29,12 +33,11 @@ def linear(p,obs):
     B0=p[0]
     B1=p[1]
     sigma=p[2]
-    
     expected=B0+B1*obs.x
     nll=-1*norm(expected,sigma).logpdf(obs.y).sum()
     return nll
     
-#set intial guesses
+#set initial guesses
 linearGuess=numpy.array([10,6,1])
 #estimate parameters
 linearFit=minimize(linear,linearGuess,method="Nelder-Mead",options={'disp':True},args=leaves)
@@ -48,18 +51,18 @@ def hump(p,obs):
     B1=p[1]
     B2=p[2]
     sigma=p[3]
-    
     expected=B0+B1*obs.x+B2*(obs.x)^2
     nll=-1*norm(expected,sigma).logpdf(obs.y).sum()
     return nll
     
 #set initial guesses
-humpGuess=numpy.array([200,10,-.2,1])
+humpGuess=numpy.array([200,10,-0.2,1])
 
 #estimate parameters
 humpFit=minimize(hump,humpGuess,method="Nelder-Mead",options={'disp':True},args=leaves)
-print(humpyFit.x)
-print(humpyFit.fun)#nll
+print(humpFit.x)
+#print nll
+print(humpFit.fun)
 
 #difference in nll(constant vs linear)
 first_D=2*(constantFit.fun-linearFit.fun)
